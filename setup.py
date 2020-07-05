a9378c9311cb2c29d091562a2c87d418609e41f9
Vinzenz May <zu95ded@mi.fu-berlin.de>
2020-07-05 20:56:09 +0200

    running, 1st deployment

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="clearCNV-vinzenzmay", # Replace with your own username
-    version="0.0.7",
+    name="clearCNV", # Replace with your own username
+    version="0.0.1",
     author="Vinzenz May",
     author_email="Vinzenz.May@mdc-berlin.de",
     description="Clinical sequencing panel CNV caller and visualizer",
@@ -23,8 +23,8 @@ setuptools.setup(
             'clearCNV = clearCNV.__main__:main',
         ],
     },
-    install_requires=["numpy=1.18.1", "pandas=1.0.1",
-    "scipy=1.4.1", "scikit-learn=0.22.1", "seaborn=0.10.0",
-    "hmmlearn=0.2.3", "matplotlib=3.1.3", "plotly=4.5.4"]
-    python_requires='>=3.7',
+    install_requires=["numpy>=1.18.1", "pandas>=1.0.1", "scipy>=1.4.1",
+    "scikit-learn>=0.22.1", "seaborn>=0.10.0", "hmmlearn>=0.2.3",
+    "matplotlib>=3.1.3", "plotly>=4.5.4"],
+    python_requires='>=3.7'
 )
