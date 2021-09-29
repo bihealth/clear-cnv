0f65f52a85c23f10805f80d9d895749796c90a98
Vinzenz May <vinzenz.may@mdc-berlin.de>
2021-09-29 12:42:47 +0200

    add tag v0.305

--- clearCNV/util.py
+++ clearCNV/util.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-import regex as re
+import re
 from sklearn.neighbors import KernelDensity
 from sklearn.linear_model import LinearRegression
 import seaborn as sns
